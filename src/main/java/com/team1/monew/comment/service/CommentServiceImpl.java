c0b6c636dcbf83101b8cc73780d9549e72a15d34
manleKim <96470835+manleKim@users.noreply.github.com>
2025-05-29 18:48:55 +0900

    feat: KAN-49 댓글 등록 (#9)
    * feat: KAN-56 댓글 서비스 인터페이스 선언 및 usecase 생성
    
    * feat: KAN-57 Comment JPA 레포지토리 구현
    
    * feat: KAN-154 CommentMapper 구현
    
    * feat: KAN-142 CommentServiceImpl register 구현
    
    * refactor: KAN-155 repository 패키지 이동
    
    * modify: KAN-156 에러 디테일 추가
    
    * feat: repository 어노테이션 추가
    
    * feat: KAN-144 댓글 생성 컨트롤러 추가
    
    * feat: KAN-145 CommentRegisterRequest 유효성 검사
    
    * feat: KAN-157 댓글 등록 로그 추가
    
    * fix: KAN-162 CommentLikeRepository 버그 수정
    
    * fix: KAN-165 CommentLikeRepository 쿼리메서정드 수정
    
    * test: KAN-146 댓글 등록 관련 테스트 코드 구현
    
    * feat: KAN-147 최종테스트
    
    - @Builder 사용으로 인해 초기값이 들어가지 않는 오류 해결
    
    * modify: KAN-166 코드리뷰 적용
    
    - Entity의 생성자는 protected로!

---
+++ src/main/java/com/team1/monew/comment/service/CommentServiceImpl.java
@@ -0,0 +1,62 @@
+package com.team1.monew.comment.service;
+
+import com.team1.monew.article.entity.Article;
+import com.team1.monew.article.repository.ArticleRepository;
+import com.team1.monew.comment.dto.CommentDto;
+import com.team1.monew.comment.dto.CommentRegisterRequest;
+import com.team1.monew.comment.entity.Comment;
+import com.team1.monew.comment.mapper.CommentMapper;
+import com.team1.monew.comment.repository.CommentRepository;
+import com.team1.monew.exception.ErrorCode;
+import com.team1.monew.exception.RestException;
+import com.team1.monew.user.entity.User;
+import com.team1.monew.user.repository.UserRepository;
+import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@AllArgsConstructor
+@Service
+public class CommentServiceImpl implements CommentService {
+
+    private final CommentRepository commentRepository;
+    private final ArticleRepository articleRepository;
+    private final UserRepository userRepository;
+    private final CommentMapper commentMapper;
+
+
+    @Override
+    public CommentDto register(CommentRegisterRequest request) {
+        log.info("댓글 등록 요청 - articleId: {}, userId: {}", request.articleId(), request.userId());
+
+        User user = userRepository.findById(request.userId())
+            .orElseThrow(() -> {
+                log.warn("사용자 조회 실패 - userId: {}", request.userId());
+                return new RestException(ErrorCode.NOT_FOUND, Map.of(
+                    "userId", request.userId(),
+                    "detail", "User not found"
+                ));
+            });
+
+        Article article = articleRepository.findById(request.articleId())
+            .orElseThrow(() -> {
+                log.warn("기사 조회 실패 - articleId: {}", request.articleId());
+                return new RestException(ErrorCode.NOT_FOUND, Map.of(
+                    "articleId", request.articleId(),
+                    "detail", "Article not found"
+                ));
+            });
+
+        Comment newComment = new Comment(article, user, request.content());
+
+        Comment savedComment = commentRepository.save(newComment);
+        log.info("댓글 저장 성공 - commentId: {}", savedComment.getId());
+
+        CommentDto dto = commentMapper.toDto(savedComment, request.userId());
+        log.debug("댓글 DTO 반환 - {}", dto);
+
+        return dto;
+    }
+}
