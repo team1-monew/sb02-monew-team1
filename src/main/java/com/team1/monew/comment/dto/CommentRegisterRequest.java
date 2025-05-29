5d866afcb73e9998bc088fa05d64788d4839f85c
manleKim <96470835+manleKim@users.noreply.github.com>
2025-05-29 18:48:55 +0900

    feat: KAN-49 댓글 등록 (#9)
    * feat: KAN-56 댓글 서비스 인터페이스 선언 및 usecase 생성
    
    * feat: KAN-57 Comment JPA 레포지토리 구현
    
    * feat: KAN-154 CommentMapper 구현
    
    * feat: KAN-142 CommentServiceImpl register 구현
    
    * refactor: KAN-155 repository 패키지 이동
    
    * modify: KAN-156 에러 디테일 추가
    
    * feat: repository 어노테이션 추가
    
    * feat: KAN-144 댓글 생성 컨트롤러 추가
    
    * feat: KAN-145 CommentRegisterRequest 유효성 검사
    
    * feat: KAN-157 댓글 등록 로그 추가
    
    * fix: KAN-162 CommentLikeRepository 버그 수정
    
    * fix: KAN-165 CommentLikeRepository 쿼리메서정드 수정
    
    * test: KAN-146 댓글 등록 관련 테스트 코드 구현
    
    * feat: KAN-147 최종테스트
    
    - @Builder 사용으로 인해 초기값이 들어가지 않는 오류 해결
    
    * modify: KAN-166 코드리뷰 적용
    
    - Entity의 생성자는 protected로!

--- src/main/java/com/team1/monew/comment/dto/CommentRegisterRequest.java
+++ src/main/java/com/team1/monew/comment/dto/CommentRegisterRequest.java
@@ -1,8 +1,18 @@
 package com.team1.monew.comment.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public record CommentRegisterRequest(
+    @NotNull(message = "기사 ID는 필수입니다.")
     Long articleId,
+
+    @NotNull(message = "사용자 ID는 필수입니다.")
     Long userId,
+
+    @NotBlank(message = "내용은 필수입니다.")
+    @Size(min=1, max=500, message = "내용은 1자 이상 500자 이하이어야 합니다.")
     String content
 ) {
 
---
+++ src/main/java/com/team1/monew/comment/mapper/CommentMapper.java
@@ -0,0 +1,31 @@
+package com.team1.monew.comment.mapper;
+
+import com.team1.monew.comment.repository.CommentLikeRepository;
+import com.team1.monew.comment.dto.CommentDto;
+import com.team1.monew.comment.entity.Comment;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class CommentMapper {
+
+    private final CommentLikeRepository commentLikeRepository;
+
+    public CommentDto toDto(Comment comment, Long userId) {
+        boolean likedByMe = commentLikeRepository.existsByComment_IdAndLikedBy_Id(
+                comment.getId(),
+                userId
+        );
+        return CommentDto.builder()
+                .id(comment.getId())
+                .articleId(comment.getArticle().getId())
+                .userId(comment.getUser().getId())
+                .userNickname(comment.getUser().getNickname())
+                .content(comment.getContent())
+                .likeCount(comment.getLikeCount())
+                .likedByMe(likedByMe)
+                .createdAt(comment.getCreatedAt())
+                .build();
+    }
+}
