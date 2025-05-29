8b1c7d4007348c88f3ad1b7cc5ef85dee6055590
manleKim <kk410910@naver.com>
2025-05-29 09:21:32 +0900

    feat: KAN-117 엔티티 구현

---
+++ src/main/java/com/team1/monew/article/entity/ArticleView.java
@@ -0,0 +1,35 @@
+package com.team1.monew.article.entity;
+
+import com.team1.monew.user.entity.User;
+import jakarta.persistence.*;
+import java.time.Instant;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "article_views")
+public class ArticleView {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "article_id", nullable = false)
+    private Article article;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User viewedBy;
+
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    public ArticleView(Article article, User viewedBy) {
+        this.article = article;
+        this.viewedBy = viewedBy;
+        this.createdAt = Instant.now();
+    }
+}
