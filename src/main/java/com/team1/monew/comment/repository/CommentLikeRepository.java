dc991dfe676a1eef7daff0a5a6784af89b1326bb
manleKim <kk410910@naver.com>
2025-05-29 14:28:08 +0900

    feat: repository 어노테이션 추가

--- src/main/java/com/team1/monew/comment/repository/CommentLikeRepository.java
+++ src/main/java/com/team1/monew/comment/repository/CommentLikeRepository.java
@@ -2,7 +2,9 @@ package com.team1.monew.comment.repository;
 
 import com.team1.monew.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CommentLikeRepository extends JpaRepository<Comment, Long> {
 
     boolean existsByCommentIdAndUserId(Long commentId, Long userId);
