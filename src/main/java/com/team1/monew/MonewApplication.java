4a91664a97ebc9cc035c7c425c48a2278764d758
manleKim <96470835+manleKim@users.noreply.github.com>
2025-05-29 09:17:19 +0900

    feat: KAN-138 스프링 앱 초기화 (#1)
    * chore: KAN-139 .gitignore 초기화
    
    * feat: KAN-140 스프링 앱 초기화

---
+++ src/main/java/com/team1/monew/MonewApplication.java
@@ -0,0 +1,13 @@
+package com.team1.monew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonewApplication.class, args);
+	}
+
+}
