682021c6ce557bda883e235c116dda9d4427ca75
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-05-29 14:48:48 +0900

    feat: KAN-148 관심사 Service, Repository 인터페이스 구현 (#8)
    * refactor: KAN-152 예외 게층 수정
    
    * feat: KAN-149 InterestService 인터페이스 구현
    
    * feat: KAN-150 InterestRepository JPA 인터페이스 구현
    
    * feat: KAN-158 SubscriptionService 인터페이스 구현
    
    * feat: KAN-159 SubscriptionRepository JPA 인터페이스 구현

--- src/main/java/com/team1/monew/exception/GlobalExceptionHandler.java
+++ src/main/java/com/team1/monew/exception/GlobalExceptionHandler.java
@@ -1,7 +1,10 @@
 package com.team1.monew.exception;
 
+import java.time.Instant;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,11 +14,22 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
   @ExceptionHandler(Exception.class)
-  public ResponseEntity<ErrorResponse> handleException(Exception e) {
-    ErrorResponse errorResponse = new ErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR, Map.of());
+  public ResponseEntity<ErrorResponse> handleUnexpectedException(Exception e) {
     log.error("예상치 못한 서버 내부 오류 처리: {} ", e.getMessage(), e);
+    ErrorResponse errorResponse = new ErrorResponse(
+        Instant.now(),
+        ErrorCode.INTERNAL_SERVER_ERROR.getStatus().value(),
+        "예상치 못한 서버 내부 오류가 발생했습니다.",
+        Map.of(),
+        e.getClass().getSimpleName(),
+        ErrorCode.INTERNAL_SERVER_ERROR.getStatus().name()
+    );
     return ResponseEntity.status(ErrorCode.INTERNAL_SERVER_ERROR.getStatus()).body(errorResponse);
   }
 
-
+  @ExceptionHandler(RestException.class)
+  public ResponseEntity<ErrorResponse> handleRestException(RestException e) {
+    log.warn("RestException 예외 처리 - errorCode: {}", e.getErrorCode().name());
+    return ResponseEntity.status(e.getErrorCode().getStatus()).body(new ErrorResponse(e));
+  }
 }
