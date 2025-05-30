9aadb9232d7ea554d47705780a73ade74f801f60
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-05-30 16:40:32 +0900

    Refactor: KNA-192 Interest, Subscription Instant -> LocalDateTime 변경

--- src/main/java/com/team1/monew/useractivity/dto/UserActivityDto.java
+++ src/main/java/com/team1/monew/useractivity/dto/UserActivityDto.java
@@ -3,7 +3,7 @@ package com.team1.monew.useractivity.dto;
 import com.team1.monew.article.entity.ArticleView;
 import com.team1.monew.comment.entity.Comment;
 import com.team1.monew.comment.entity.CommentLike;
-import com.team1.monew.subscription.entity.Subscription;
+import com.team1.monew.interest.entity.Subscription;
 import java.time.Instant;
 import java.util.List;
 
