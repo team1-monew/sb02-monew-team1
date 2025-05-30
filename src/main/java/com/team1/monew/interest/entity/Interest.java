ce05477c52c87259e4f33e6303d20c3ff9a68763
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-05-30 18:39:01 +0900

    feat: KAN-43 관심사 등록 (#14)
    * refactor: KAN-164 Interest, Keyword, Subscription 엔티티 수정
    
    * refactor: KAN-164 InterestRepository 페치 조인 추가
    
    * feat: KAN-46 관심사 등록 API 구현
    
    * feat: KNA-160 InterestService create 메서드 단위 테스트 작성
    
    * refactor: KNA-192 Interest, Subscription Instant -> LocalDateTime 변경
    
    * refactor: KAN-196 관심사 등록 리팩토링

--- src/main/java/com/team1/monew/interest/entity/Interest.java
+++ src/main/java/com/team1/monew/interest/entity/Interest.java
@@ -1,19 +1,24 @@
 package com.team1.monew.interest.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "interests")
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Interest {
 
     @Id
@@ -27,7 +32,22 @@ public class Interest {
     private Long subscriberCount = 0L;
 
     @Column(nullable = false, updatable = false)
-    private Instant createdAt;
+    private LocalDateTime createdAt;
+
+    // Interest가 save되면, 거기에 맞춰서 keyword가 save / remove됨 (Cascade, orphanRemoval)
+    @OneToMany(mappedBy = "interest", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Keyword> keywords = new ArrayList<>();
+
+    // 양방향 연관관계 편의 메서드
+    public void addKeyword(Keyword keyword) {
+        keywords.add(keyword);
+        keyword.updateInterest(this);
+    }
+
+    public void updateKeywords(List<Keyword> keywords) {
+        this.keywords.clear();
+        keywords.forEach(this::addKeyword);
+    }
 
     public void updateSubscriberCount(Long count) {
         this.subscriberCount = count;
@@ -35,7 +55,7 @@ public class Interest {
 
     public Interest(String name) {
         this.name = name;
-        this.createdAt = Instant.now();
+        this.createdAt = LocalDateTime.now();
     }
 
 }
