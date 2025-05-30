d86079b55518bd503ca82d49271d40056e5bf66d
manleKim <96470835+manleKim@users.noreply.github.com>
2025-05-30 17:38:33 +0900

    modify: KAN-191 댓글 엔티티와 DTO Instant -> LocalDateTime으로 변경 (#12)

--- src/test/java/com/team1/monew/comment/repository/CommentLikeRepositoryTest.java
+++ src/test/java/com/team1/monew/comment/repository/CommentLikeRepositoryTest.java
@@ -7,7 +7,7 @@ import com.team1.monew.article.entity.Article;
 import com.team1.monew.comment.entity.Comment;
 import com.team1.monew.comment.entity.CommentLike;
 import com.team1.monew.user.entity.User;
-import java.time.Instant;
+import java.time.LocalDateTime;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -30,7 +30,7 @@ public class CommentLikeRepositoryTest {
         User user = new User("test@example.com", "닉네임", "암호");
         entityManager.persist(user);
 
-        Article article = new Article("source", "url", "제목", Instant.now(), "요약");
+        Article article = new Article("source", "url", "제목", LocalDateTime.now(), "요약");
         entityManager.persist(article);
 
         Comment comment = new Comment(article, user, "댓글 내용");
@@ -56,7 +56,7 @@ public class CommentLikeRepositoryTest {
         User user = new User("test2@example.com", "다른유저", "암호");
         entityManager.persist(user);
 
-        Article article = new Article("source", "url", "제목", Instant.now(), "요약");
+        Article article = new Article("source", "url", "제목", LocalDateTime.now(), "요약");
         entityManager.persist(article);
 
         Comment comment = new Comment(article, user, "다른 댓글 내용");
