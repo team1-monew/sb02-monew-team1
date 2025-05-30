be9d755437ceed3141aeb363f19c4fd6e3b4f3d9
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-05-30 13:12:09 +0900

    refactor: KAN-164 InterestRepository 페치 조인 추가

--- src/main/java/com/team1/monew/interest/repository/InterestRepository.java
+++ src/main/java/com/team1/monew/interest/repository/InterestRepository.java
@@ -1,7 +1,14 @@
 package com.team1.monew.interest.repository;
 
 import com.team1.monew.interest.entity.Interest;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface InterestRepository extends JpaRepository<Interest, Long> {
+
+  @Query("SELECT i FROM Interest i LEFT JOIN FETCH i.keywords WHERE i.id = :id")
+  Optional<Interest> findByIdFetch(@Param("id") Long id);
+
 }
