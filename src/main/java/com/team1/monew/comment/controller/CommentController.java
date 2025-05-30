7dcb6d5fddddb4ad8a3158144cb805821bce9298
manleKim <kk410910@naver.com>
2025-05-30 11:26:47 +0900

    feat: KAN-173 CommentController에 댓글 수정 API 진입점 추가

--- src/main/java/com/team1/monew/comment/controller/CommentController.java
+++ src/main/java/com/team1/monew/comment/controller/CommentController.java
@@ -2,13 +2,17 @@ package com.team1.monew.comment.controller;
 
 import com.team1.monew.comment.dto.CommentDto;
 import com.team1.monew.comment.dto.CommentRegisterRequest;
+import com.team1.monew.comment.dto.CommentUpdateRequest;
 import com.team1.monew.comment.service.CommentService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,5 +35,21 @@ public class CommentController {
         return ResponseEntity
                 .status(201)
                 .body(commentDto);
+    }
+
+    @PatchMapping("/{commentId}")
+    public ResponseEntity<CommentDto> update(
+            @RequestBody CommentUpdateRequest request,
+            @PathVariable Long commentId,
+            @RequestHeader("Monew-Request-User-ID") Long userId
+        ) {
+        log.info("PATCH /api/comments/{} 요청 수신 - request.content: {}, userId: {}", commentId, request.content(), userId);
+
+        CommentDto updatedCommentDto = commentService.update(request, commentId, userId);
+
+        log.info("댓글 수정 요청 성공 - commentId: {}", updatedCommentDto.id());
+
+        return ResponseEntity
+                .ok(updatedCommentDto);
     }
 }
