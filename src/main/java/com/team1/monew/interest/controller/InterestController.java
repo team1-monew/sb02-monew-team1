d3c14700722e93f08cdbe1c3725978dd73de56a1
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-05-30 19:33:32 +0900

    feat: KAN-194 관심사 수정 (#15)
    * feat: KAN-197 관심사 수정 API 구현
    
    * test: KAN-199 InterestServiceTest 단위 테스트 작성

--- src/main/java/com/team1/monew/interest/controller/InterestController.java
+++ src/main/java/com/team1/monew/interest/controller/InterestController.java
@@ -2,12 +2,15 @@ package com.team1.monew.interest.controller;
 
 import com.team1.monew.interest.dto.InterestDto;
 import com.team1.monew.interest.dto.InterestRegisterRequest;
+import com.team1.monew.interest.dto.InterestUpdateRequest;
 import com.team1.monew.interest.service.InterestService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -29,5 +32,15 @@ public class InterestController {
     log.info("관심사 생성 요청 - userId: {}, interestName: {}", userId, interestRegisterRequest.name());
     InterestDto interestDto = interestService.create(interestRegisterRequest);
     return ResponseEntity.status(HttpStatus.CREATED).body(interestDto);
+  }
+
+  @PatchMapping("/{interestId}")
+  public ResponseEntity<InterestDto> update(
+      @RequestBody @Valid InterestUpdateRequest interestUpdateRequest,
+      @PathVariable("interestId") Long interestId,
+      @RequestHeader("Monew-Request-User-ID") Long userId) {
+    log.info("관심사 수정 요청 - userId: {}, interestId: {}", userId, interestId);
+    InterestDto interestDto = interestService.update(interestId, interestUpdateRequest);
+    return ResponseEntity.status(HttpStatus.OK).body(interestDto);
   }
 }
