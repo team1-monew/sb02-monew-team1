cdf3b2fae49cb77916c3deb64b1db268cb44a327
manleKim <96470835+manleKim@users.noreply.github.com>
2025-05-30 13:08:23 +0900

    feat: KAN-168 댓글 수정 (#10)
    * refactor: KAN-169 CommentMapper에 의존성 제거 및 테스트 수정
    
    - 단순 mapper class에 의존성 최대한 피하기
    
    * feat: KAN-171 댓글 수정 유스케이스 추가
    
    * feat: KAN-172 CommentServiceImpl에 update 메서드 추가
    
    * feat: KAN-173 CommentController에 댓글 수정 API 진입점 추가
    
    * feat: KAN-174 CommentUpdateRequest 유효성 검사 추가
    
    * test: KAN-176 댓글 수정 테스트 코드 구현

--- src/main/java/com/team1/monew/comment/service/CommentService.java
+++ src/main/java/com/team1/monew/comment/service/CommentService.java
@@ -1,9 +1,8 @@
 package com.team1.monew.comment.service;
 
 import com.team1.monew.comment.service.usecase.RegisterCommentUseCase;
+import com.team1.monew.comment.service.usecase.UpdateCommentUseCase;
 
-public interface CommentService extends RegisterCommentUseCase {
-
-
+public interface CommentService extends RegisterCommentUseCase, UpdateCommentUseCase {
 
 }
--- src/test/java/com/team1/monew/comment/repository/CommentLikeRepositoryTest.java
+++ src/test/java/com/team1/monew/comment/repository/CommentLikeRepositoryTest.java
@@ -12,7 +12,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @DataJpaTest
 public class CommentLikeRepositoryTest {
 
@@ -26,14 +28,12 @@ public class CommentLikeRepositoryTest {
     void existsByCommentIdAndLikedBy_존재하면_true반환() {
         // given
         User user = new User("test@example.com", "닉네임", "암호");
-//        ReflectionTestUtils.setField(user, "id", 1L);
         entityManager.persist(user);
 
         Article article = new Article("source", "url", "제목", Instant.now(), "요약");
         entityManager.persist(article);
 
         Comment comment = new Comment(article, user, "댓글 내용");
-//        ReflectionTestUtils.setField(comment, "id", 10L);
         entityManager.persist(comment);
 
         CommentLike like = new CommentLike(comment, user);
