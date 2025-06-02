b7ca9b6f54cce311029a907f10a573126c41a3b9
Leekb0804 <lkb8713@gmail.com>
2025-06-02 09:49:36 +0900

    feat: KAN-16 사용자 등록 및 로그인 (#13)
    * feat: KAN-19 사용자 엔티티 및 DB 설계 (이메일, 닉네임, 비밀번호)
    
    * feat: KAN-21 회원가입 API (중복 이메일 불가)
    
    * feat: KAN-22 로그인 API (이메일+비밀번호 검증)
    
    * refactor: KAN-202 PR 수정 내용 반영
    
    - 내부 검증 로직 private 으로 변경 및 그에 따른 테스트 코드 삭제

---
+++ src/main/java/com/team1/monew/user/controller/UserController.java
@@ -0,0 +1,46 @@
+package com.team1.monew.user.controller;
+
+import com.team1.monew.user.dto.UserDto;
+import com.team1.monew.user.dto.UserLoginRequest;
+import com.team1.monew.user.dto.UserRegisterRequest;
+import com.team1.monew.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+  private final UserService userService;
+
+  @PostMapping
+  public ResponseEntity<UserDto> create(
+      @RequestBody UserRegisterRequest userRegisterRequest
+  ) {
+    log.info("회원가입 요청: email={}, nickname={}", userRegisterRequest.email(), userRegisterRequest.nickname());
+    UserDto createdUser = userService.createUser(userRegisterRequest);
+    log.debug("회원가입 응답: {}", createdUser);
+    return ResponseEntity
+        .status(HttpStatus.CREATED)
+        .body(createdUser);
+  }
+
+  @PostMapping("/login")
+  public ResponseEntity<UserDto> login(
+      @RequestBody UserLoginRequest userLoginRequest
+  ) {
+    log.info("로그인 요청: email={}", userLoginRequest.email());
+    UserDto createdUser = userService.login(userLoginRequest);
+    log.debug("로그인 응답: {}", createdUser);
+    return ResponseEntity
+        .status(HttpStatus.OK)
+        .body(createdUser);
+  }
+}
