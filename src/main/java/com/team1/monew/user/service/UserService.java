0920cdaf68822109a2632cb93b6ec66d6cf287ce
Leekb0804 <lkb8713@gmail.com>
2025-06-02 16:17:26 +0900

    feat: KAN-17 사용자 정보 수정 / 삭제 (#18)
    * feat: KAN-25 닉네임 수정 API 구현
    
    * feat: KAN-26 논리 삭제 API (is_deleted 플래그)
    
    * feat: KAN-27 물리 삭제 API 및 연관 정보 처리

--- src/main/java/com/team1/monew/user/service/UserService.java
+++ src/main/java/com/team1/monew/user/service/UserService.java
@@ -3,8 +3,12 @@ package com.team1.monew.user.service;
 import com.team1.monew.user.dto.UserDto;
 import com.team1.monew.user.dto.UserLoginRequest;
 import com.team1.monew.user.dto.UserRegisterRequest;
+import com.team1.monew.user.dto.UserUpdateRequest;
 
 public interface UserService {
   UserDto createUser(UserRegisterRequest userRegisterRequest);
   UserDto login(UserLoginRequest userLoginRequest);
+  UserDto updateUser(Long id, UserUpdateRequest userUpdateRequest);
+  void deleteUser(Long id);
+  void deleteUserHard(Long id);
 }
