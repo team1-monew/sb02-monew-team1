d02f5a37a35e270179350ee6ceb8d30e1f2b7a58
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-06-03 17:26:19 +0900

    feat: KAN-76 관심사 조회 및 검색 (정렬 + 커서 페이지네이션) (#21)
    * chore: KAN-222 Querydsl 초기 세팅
    
    * feat: KAN-87 관심사 조회 및 검색 API 구현 (커서 페이지네이션)
    
    * test: KAN-210 InterestRepository 단위테스트 작성
    
    * test: KAN-210 InterestRepositoryCustomImpl 단위테스트 작성
    
    * test: KAN-210 InterestService findInterestsWithCursor 메서드 단위테스트 작성

--- src/main/java/com/team1/monew/interest/controller/InterestController.java
+++ src/main/java/com/team1/monew/interest/controller/InterestController.java
@@ -1,21 +1,31 @@
 package com.team1.monew.interest.controller;
 
+import com.team1.monew.common.dto.CursorPageResponse;
 import com.team1.monew.interest.dto.InterestDto;
 import com.team1.monew.interest.dto.InterestRegisterRequest;
+import com.team1.monew.interest.dto.InterestSearchCondition;
 import com.team1.monew.interest.dto.InterestUpdateRequest;
+import com.team1.monew.interest.mapper.PageResponseMapper;
 import com.team1.monew.interest.service.InterestService;
 import jakarta.validation.Valid;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Slice;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,6 +35,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class InterestController {
 
   private final InterestService interestService;
+  private final PageResponseMapper pageResponseMapper;
 
   @PostMapping
   public ResponseEntity<InterestDto> create(
@@ -34,6 +45,33 @@ public class InterestController {
     InterestDto interestDto = interestService.create(interestRegisterRequest);
     return ResponseEntity.status(HttpStatus.CREATED).body(interestDto);
   }
+
+  @GetMapping
+  public ResponseEntity<CursorPageResponse<InterestDto>> getInterests(
+      @RequestParam(name = "direction", defaultValue = "DESC") String direction,
+      @RequestParam(name = "limit", defaultValue = "10") int limit,
+      @RequestParam(name = "orderBy", defaultValue = "subscriberCount") String orderBy,
+      @RequestParam(name = "keyword", required = false) String keyword,
+      @RequestParam(name = "cursor", required = false) String cursor,
+      // 프론트단에서는 after를 백엔드에서 받은 nextAfter값을 가지고 있다가 넘겨줌.
+      // 즉, 백엔드에서 nextAfter를 LocalDateTime 타입으로 넘겨준다면, after도 LocalDateTime으로 넘겨줄 것
+      @RequestParam(name = "after", required = false) LocalDateTime after,
+      @RequestHeader("Monew-Request-User-ID") Long userId) {
+    InterestSearchCondition interestSearchCondition = InterestSearchCondition.builder()
+        .keyword(keyword)
+        .cursor(cursor)
+        .direction(direction)
+        .limit(limit)
+        .orderBy(orderBy)
+        .after(after)
+        .build();
+
+    Slice<InterestDto> interestDtoList = interestService.findInterestsWithCursor(
+        interestSearchCondition);
+    return ResponseEntity.status(HttpStatus.OK)
+        .body(pageResponseMapper.toPageResponse(interestDtoList, interestSearchCondition));
+  }
+
 
   @PatchMapping("/{interestId}")
   public ResponseEntity<InterestDto> update(
