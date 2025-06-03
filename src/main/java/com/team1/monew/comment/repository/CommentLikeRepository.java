6037ffcfdb41f46aa7258eac8fdd5ee4edd399d7
manleKim <96470835+manleKim@users.noreply.github.com>
2025-06-03 09:13:31 +0900

    feat: KAN-52 댓글 좋아요 기능 추가 (#20)
    * feat: KAN-204 CommentLikeService 인터페이스 추가
    
    * feat: KAN-205 CommentLikeService 구현체 구현
    
    * feat: KAN-206 댓글 좋아요 관련 컨트롤러 추가
    
    * test: KAN-208 댓글 좋아요 관련 테스트 코드 추가

--- src/main/java/com/team1/monew/comment/repository/CommentLikeRepository.java
+++ src/main/java/com/team1/monew/comment/repository/CommentLikeRepository.java
@@ -1,11 +1,16 @@
 package com.team1.monew.comment.repository;
 
 import com.team1.monew.comment.entity.CommentLike;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> {
+
+    Long countByCommentId(Long commentId);
+
+    Optional<CommentLike> findByComment_IdAndLikedBy_Id(Long commentId, Long userId);
 
     boolean existsByComment_IdAndLikedBy_Id(Long commentId, Long userId);
 
