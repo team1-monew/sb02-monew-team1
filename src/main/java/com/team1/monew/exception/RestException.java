542b5e1a9d7297791cb2c6a4d8e4c06f4d37a6d7
nayu-yuna <dldldbsk@naver.com>
2025-06-04 18:16:40 +0900

    refactor: KAN-235 코드 리팩토링

--- src/main/java/com/team1/monew/exception/RestException.java
+++ src/main/java/com/team1/monew/exception/RestException.java
@@ -1,6 +1,6 @@
 package com.team1.monew.exception;
 
-import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.Map;
 import lombok.Getter;
@@ -8,20 +8,20 @@ import lombok.Getter;
 @Getter
 public class RestException extends RuntimeException {
 
-  private final Instant timestamp;
+  private final LocalDateTime timestamp;
   private final ErrorCode errorCode;
   private final Map<String, Object> details;
 
   public RestException(ErrorCode errorCode) {
     super(errorCode.getMessage());
-    this.timestamp = Instant.now();
+    this.timestamp = LocalDateTime.now();
     this.errorCode = errorCode;
     this.details = Collections.emptyMap();
   }
 
   public RestException(ErrorCode errorCode, Map<String, Object> details) {
     super(errorCode.getMessage());
-    this.timestamp = Instant.now();
+    this.timestamp = LocalDateTime.now();
     this.errorCode = errorCode;
     this.details = details;
   }
--- src/main/java/com/team1/monew/notification/dto/NotificationDto.java
+++ src/main/java/com/team1/monew/notification/dto/NotificationDto.java
@@ -1,11 +1,11 @@
 package com.team1.monew.notification.dto;
 
-import java.time.Instant;
+import java.time.LocalDateTime;
 
 public record NotificationDto(
     Long id,
-    Instant createdAt,
-    Instant updatedAt,
+    LocalDateTime createdAt,
+    LocalDateTime updatedAt,
     boolean confirmed,
     Long userId,
     String content,
