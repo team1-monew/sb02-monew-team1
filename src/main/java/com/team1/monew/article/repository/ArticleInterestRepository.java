d807f3b1d4579bf5d9efe0459449675527412e9b
nayu-yuna <dldldbsk@naver.com>
2025-06-04 15:59:16 +0900

    feat: KAN-38 뉴스 기사 조회 (#25)
    * feat: KAN-80 기사 조회 기록 기능 구현
    
    * feat: KAN-81 기사 목록 조회 기능 구현
    
    * feat: KAN-82 출처 목록 조회 기능 구현

--- src/main/java/com/team1/monew/article/repository/ArticleInterestRepository.java
+++ src/main/java/com/team1/monew/article/repository/ArticleInterestRepository.java
@@ -3,6 +3,8 @@ package com.team1.monew.article.repository;
 import com.team1.monew.article.entity.ArticleInterest;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ArticleInterestRepository extends CrudRepository<ArticleInterest, Long> {
 
   void deleteByArticleId(Long articleId);
