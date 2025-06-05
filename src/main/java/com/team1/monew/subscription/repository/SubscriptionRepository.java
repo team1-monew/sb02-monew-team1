3993b612158fb25ea4d750cbbd6125695f3a3dac
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-06-05 14:19:52 +0900

    feat: KAN-250 interestDto에 구독여부 반영

--- src/main/java/com/team1/monew/subscription/repository/SubscriptionRepository.java
+++ src/main/java/com/team1/monew/subscription/repository/SubscriptionRepository.java
@@ -1,12 +1,20 @@
 package com.team1.monew.subscription.repository;
 
 import com.team1.monew.subscription.entity.Subscription;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
 
   Optional<Subscription> findByInterest_IdAndUser_Id(Long interestId, Long UserId);
   boolean existsByInterest_IdAndUser_Id(Long interestId, Long UserId);
 
+  @Query("SELECT s.interest.id FROM Subscription s WHERE s.user.id = :userId")
+  List<Long> findSubscribedInterestIdByUserId(Long userId);
+
+  // 활동내역 구독 조회할 때 쓸 것
+  @Query("SELECT s FROM Subscription s LEFT JOIN FETCH s.interest WHERE s.user.id = :userId")
+  List<Subscription> findByUserIdFetch(Long userId);
 }
