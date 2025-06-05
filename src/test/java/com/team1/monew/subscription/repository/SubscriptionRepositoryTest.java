cceb571ce2e0dbf1ceab12eb5145695e2add16c9
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-06-05 14:25:26 +0900

    test: KAN-251 테스트코드 수정

--- src/test/java/com/team1/monew/subscription/repository/SubscriptionRepositoryTest.java
+++ src/test/java/com/team1/monew/subscription/repository/SubscriptionRepositoryTest.java
@@ -5,6 +5,8 @@ import com.team1.monew.interest.entity.Interest;
 import com.team1.monew.subscription.entity.Subscription;
 import com.team1.monew.user.entity.User;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceUnitUtil;
+import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -20,6 +22,7 @@ import static org.assertj.core.api.Assertions.*;
 @ActiveProfiles("test")
 @Import(QueryDslConfig.class)
 public class SubscriptionRepositoryTest {
+
   @Autowired
   private SubscriptionRepository subscriptionRepository;
 
@@ -30,7 +33,7 @@ public class SubscriptionRepositoryTest {
   @DisplayName("관심사 ID와 유저 ID로 구독 조회 성공")
   void findByInterestIdAndUserId_success() {
     // given
-    User user = new User("test@test.com","testUser", "1234");
+    User user = new User("test@test.com", "testUser", "1234");
     Interest interest = new Interest("테스트");
     entityManager.persist(user);
     entityManager.persist(interest);
@@ -54,7 +57,7 @@ public class SubscriptionRepositoryTest {
   @DisplayName("관심사 ID와 유저 ID로 구독 존재 여부 확인")
   void existsByInterestIdAndUserId_success() {
     // given
-    User user = new User("test@test.com","testUser", "1234");
+    User user = new User("test@test.com", "testUser", "1234");
     Interest interest = new Interest("테스트");
     entityManager.persist(user);
     entityManager.persist(interest);
@@ -80,5 +83,64 @@ public class SubscriptionRepositoryTest {
 
     // then
     assertThat(found).isNotPresent();
+  }
+
+  @Test
+  @DisplayName("유저가 구독한 관심사의 ID 리스트 조회")
+  void findSubscribedInterestId_byUserId_success() {
+    // given
+    User user = new User("test@test.com", "testUser", "1234");
+    Interest interest1 = new Interest("테스트1");
+    Interest interest2 = new Interest("테스트2");
+    entityManager.persist(user);
+    entityManager.persist(interest1);
+    entityManager.persist(interest2);
+
+    Subscription subscription1 = new Subscription(user, interest1);
+    Subscription subscription2 = new Subscription(user, interest2);
+    entityManager.persist(subscription1);
+    entityManager.persist(subscription2);
+    entityManager.flush();
+    entityManager.clear();
+
+    // when
+    List<Long> interestIdList = subscriptionRepository.findSubscribedInterestIdByUserId(
+        user.getId());
+
+    // then
+    assertThat(interestIdList).hasSize(2);
+    assertThat(interestIdList).containsExactlyInAnyOrderElementsOf(
+        List.of(interest1.getId(), interest2.getId()));
+  }
+
+  @Test
+  @DisplayName("유저가 구독한 관심사 전체 리스트 fetch join 조회")
+  void findSubscriptions_byUserId_fetchJoin_success() {
+    // given
+    PersistenceUnitUtil util = entityManager.getEntityManagerFactory().getPersistenceUnitUtil();
+    User user = new User("test@test.com", "testUser", "1234");
+    Interest interest1 = new Interest("테스트1");
+    Interest interest2 = new Interest("테스트2");
+    entityManager.persist(user);
+    entityManager.persist(interest1);
+    entityManager.persist(interest2);
+
+    Subscription subscription1 = new Subscription(user, interest1);
+    Subscription subscription2 = new Subscription(user, interest2);
+    entityManager.persist(subscription1);
+    entityManager.persist(subscription2);
+    entityManager.flush();
+    entityManager.clear();
+
+    // when
+    List<Subscription> subscriptionList = subscriptionRepository.findByUserIdFetch(user.getId());
+
+    List<String> subscribedInterestNameList = subscriptionList.stream()
+        .map(subscription -> subscription.getInterest().getName()).toList();
+
+    // then
+    assertThat(subscriptionList).hasSize(2);
+    assertThat(subscribedInterestNameList).containsExactlyInAnyOrderElementsOf(
+        List.of(interest1.getName(), interest2.getName()));
   }
 }
