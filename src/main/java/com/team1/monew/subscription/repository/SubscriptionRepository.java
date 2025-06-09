727ccc064baaf604450a4e1de8bc75a0dbd896e9
Leekb0804 <lkb8713@gmail.com>
2025-06-09 18:28:48 +0900

    feat: KAN-291 관심 기사 알림 관련 서비스, 레포지토리 구현

--- src/main/java/com/team1/monew/subscription/repository/SubscriptionRepository.java
+++ src/main/java/com/team1/monew/subscription/repository/SubscriptionRepository.java
@@ -1,10 +1,12 @@
 package com.team1.monew.subscription.repository;
 
+import com.team1.monew.interest.entity.Interest;
 import com.team1.monew.subscription.entity.Subscription;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
 
@@ -17,4 +19,8 @@ public interface SubscriptionRepository extends JpaRepository<Subscription, Long
   // 활동내역 구독 조회할 때 쓸 것
   @Query("SELECT s FROM Subscription s LEFT JOIN FETCH s.interest WHERE s.user.id = :userId")
   List<Subscription> findByUserIdFetch(Long userId);
+
+  // 알림관리에서 사용
+  @Query("SELECT s FROM Subscription s JOIN FETCH s.user WHERE s.interest = :interest")
+  List<Subscription> findAllByInterestWithUser(@Param("interest") Interest interest);
 }
