a18387ed114fdc73c5baba42285867617e79a05e
Leekb0804 <lkb8713@gmail.com>
2025-06-09 18:49:01 +0900

    feat: KAN-68 관심사 관련 기사 등록 시 알림 생성 트리거 (#42)
    * feat: KAN-290 관심 기사 알림 이벤트 구현
    
    * feat: KAN-291 관심 기사 알림 관련 서비스, 레포지토리 구현
    
    * feat: KAN-292 테스트코드 작성

--- src/main/java/com/team1/monew/notification/event/NotificationEventListener.java
+++ src/main/java/com/team1/monew/notification/event/NotificationEventListener.java
@@ -1,7 +1,13 @@
 package com.team1.monew.notification.event;
 
+import com.team1.monew.article.event.NewArticlesCollectedEvent;
 import com.team1.monew.comment.event.CommentLikedEvent;
+import com.team1.monew.interest.entity.Interest;
 import com.team1.monew.notification.service.NotificationService;
+import com.team1.monew.subscription.entity.Subscription;
+import com.team1.monew.subscription.repository.SubscriptionRepository;
+import com.team1.monew.user.entity.User;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
@@ -14,6 +20,32 @@ import org.springframework.stereotype.Component;
 public class NotificationEventListener {
 
   private final NotificationService notificationService;
+
+  private final SubscriptionRepository subscriptionRepository;
+
+  @Async
+  @EventListener
+  public void handleNewArticlesEvent(NewArticlesCollectedEvent event) {
+    log.info("새로운 기사 이벤트 처리 중 - 관심사 이름: {}, 기사 개수: {}",
+        event.getInterest().getName(),
+        event.getArticles().size());
+
+    Interest interest = event.getInterest();
+    List<User> subscribers = subscriptionRepository.findAllByInterestWithUser(interest)
+        .stream()
+        .map(Subscription::getUser)
+        .toList();
+
+    try {
+      for (User user : subscribers) {
+        notificationService.notifyNewArticles(user, interest, event.getArticles().size());
+      }
+      log.info("새 기사 수집 알림 전송 완료 - 관심사 ID: {}, 알림 대상 사용자 수: {}",
+          interest.getId(), subscribers.size());
+    } catch (Exception e) {
+      log.error("새 기사 수집 알림 전송 실패 - 관심사 ID: {}", interest.getId(), e);
+    }
+  }
 
   @Async
   @EventListener
