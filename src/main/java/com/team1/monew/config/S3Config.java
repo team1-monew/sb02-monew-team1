237e3002e075a5ac355aa92b79c6f49f6ba07982
nayu-yuna <dldldbsk@naver.com>
2025-06-09 14:34:00 +0900

    feat: KAN-264 AWS S3 관련 기능 구현 (#40)

---
+++ src/main/java/com/team1/monew/config/S3Config.java
@@ -0,0 +1,32 @@
+package com.team1.monew.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class S3Config {
+
+    @Value("${AWS_S3_ACCESS_KEY}")
+    private String accessKeyId;
+
+    @Value("${AWS_S3_SECRET_KEY}")
+    private String secretKey;
+
+    @Value("${AWS_S3_REGION}")
+    private String region;
+
+    @Bean
+    public S3Client s3Client() {
+        AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(accessKeyId, secretKey);
+
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
+                .build();
+    }
+}
