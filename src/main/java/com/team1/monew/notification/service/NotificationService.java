6c09e01d8d2e15d8ab8fcf9f84c82a1020518b2c
Leekb0804 <lkb8713@gmail.com>
2025-06-10 18:05:45 +0900

    feat: KAN-256 알림 목록 조회 (#45)
    * feat: KAN-303 엔티티, dto 및 유틸 클래스 작성
    
    * feat: KAN-259 컨트롤러 구현
    
    * feat: KAN-260 서비스 구현
    
    * feat: KAN-261 레포지토리 구현
    
    * feat: KAN-262 테스트코드 작성

--- src/main/java/com/team1/monew/notification/service/NotificationService.java
+++ src/main/java/com/team1/monew/notification/service/NotificationService.java
@@ -1,10 +1,14 @@
 package com.team1.monew.notification.service;
 
 import com.team1.monew.comment.entity.Comment;
+import com.team1.monew.common.dto.CursorPageResponse;
 import com.team1.monew.interest.entity.Interest;
+import com.team1.monew.notification.dto.NotificationCursorRequest;
+import com.team1.monew.notification.dto.NotificationDto;
 import com.team1.monew.user.entity.User;
 
 public interface NotificationService {
   void notifyNewArticles(User user, Interest interest, int articleCount);
   void notifyCommentLiked(Comment comment, User likedBy);
+  CursorPageResponse<NotificationDto> getAllNotifications(NotificationCursorRequest request);
 }
--- src/test/java/com/team1/monew/notification/service/NotificationServiceImplTest.java
+++ src/test/java/com/team1/monew/notification/service/NotificationServiceImplTest.java
@@ -1,26 +1,41 @@
 package com.team1.monew.notification.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
 import com.team1.monew.comment.entity.Comment;
+import com.team1.monew.common.dto.CursorPageResponse;
 import com.team1.monew.interest.entity.Interest;
+import com.team1.monew.notification.dto.NotificationCursorRequest;
+import com.team1.monew.notification.dto.NotificationDto;
 import com.team1.monew.notification.dto.ResourceType;
 import com.team1.monew.notification.entity.Notification;
+import com.team1.monew.notification.mapper.NotificationPageResponseMapper;
 import com.team1.monew.notification.repository.NotificationRepository;
 import com.team1.monew.user.entity.User;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 import org.springframework.test.util.ReflectionTestUtils;
 
 @ExtendWith(MockitoExtension.class)
 class NotificationServiceImplTest {
   @Mock
   NotificationRepository notificationRepository;
+
+  @Mock
+  private NotificationPageResponseMapper notificationPageResponseMapper;
 
   @InjectMocks
   NotificationServiceImpl notificationService;
@@ -69,5 +84,49 @@ class NotificationServiceImplTest {
     assertThat(saved.getUser()).isEqualTo(commentAuthor);
     assertThat(saved.getResourceType()).isEqualTo(ResourceType.COMMENT.getName());
     assertThat(saved.getResourceId()).isEqualTo(comment.getId());
+  }
+
+  @Test
+  void getAllNotifications() {
+    // given
+    NotificationCursorRequest request = NotificationCursorRequest.builder()
+        .userId(1L)
+        .direction("DESC")
+        .cursor(null)
+        .after(null)
+        .limit(10)
+        .build();
+
+    List<NotificationDto> dtoList = List.of(
+        NotificationDto.builder()
+            .id(11L)
+            .createdAt(null)
+            .updatedAt(null)
+            .confirmed(false)
+            .userId(1L)
+            .content("content")
+            .resourceType(ResourceType.COMMENT.getName())
+            .resourceId(null)
+            .build()
+        );
+
+    Slice<NotificationDto> slice = new SliceImpl<>(dtoList, PageRequest.of(0, request.limit()), false);
+
+    CursorPageResponse<NotificationDto> expectedResponse =
+        new CursorPageResponse<>(dtoList, null, null, 1L, null, false);
+
+    // stub
+    given(notificationRepository.getAllByCursorRequest(any())).willReturn(slice);
+    given(notificationPageResponseMapper.toPageResponse(anyList(), any(), anyBoolean()))
+        .willReturn(expectedResponse);
+
+    // when
+    CursorPageResponse<NotificationDto> result = notificationService.getAllNotifications(request);
+
+    // then
+    assertThat(result).isEqualTo(expectedResponse);
+    verify(notificationRepository).getAllByCursorRequest(request);
+    verify(notificationPageResponseMapper)
+        .toPageResponse(dtoList, request, false);
   }
 }
