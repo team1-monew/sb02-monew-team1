35c0526e877d9de219ef5a4fe7f7d6df82855163
Leekb0804 <lkb8713@gmail.com>
2025-06-10 17:35:43 +0900

    feat: KAN-303 엔티티, dto 및 유틸 클래스 작성

--- src/main/java/com/team1/monew/notification/dto/NotificationDto.java
+++ src/main/java/com/team1/monew/notification/dto/NotificationDto.java
@@ -12,5 +12,63 @@ public record NotificationDto(
     String resourceType,
     Long resourceId
 ) {
+  public static class Builder {
+    private Long id;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private boolean confirmed;
+    private Long userId;
+    private String content;
+    private String resourceType;
+    private Long resourceId;
 
+
+    public Builder id(Long id) {
+      this.id = id;
+      return this;
+    }
+
+    public Builder createdAt(LocalDateTime createdAt) {
+      this.createdAt = createdAt;
+      return this;
+    }
+
+    public Builder updatedAt(LocalDateTime updatedAt) {
+      this.updatedAt = updatedAt;
+      return this;
+    }
+
+    public Builder confirmed(boolean confirmed) {
+      this.confirmed = confirmed;
+      return this;
+    }
+
+    public Builder userId(Long userId) {
+      this.userId = userId;
+      return this;
+    }
+
+    public Builder content(String content) {
+      this.content = content;
+      return this;
+    }
+
+    public Builder resourceType(String resourceType) {
+      this.resourceType = resourceType;
+      return this;
+    }
+
+    public Builder resourceId(Long resourceId) {
+      this.resourceId = resourceId;
+      return this;
+    }
+
+    public NotificationDto build() {
+      return new NotificationDto(id, createdAt, updatedAt, confirmed, userId, content, resourceType, resourceId);
+    }
+  }
+
+  public static Builder builder() {
+    return new Builder();
+  }
 }
