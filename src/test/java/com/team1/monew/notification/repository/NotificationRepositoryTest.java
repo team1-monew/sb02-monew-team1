aa6c42581e520f1f9831055f3a4c0280571fd422
Leekb0804 <lkb8713@gmail.com>
2025-06-11 10:26:07 +0900

    feat: KAN-257 전체 알림 확인 (#46)
    * feat: KAN-307 컨트롤러 구현
    
    * feat: KAN-308 서비스 구현
    
    * feat: KAN-309 레포지토리 구현
    
    * feat: KAN-310 테스트 코드 작성

---
+++ src/test/java/com/team1/monew/notification/repository/NotificationRepositoryTest.java
@@ -0,0 +1,68 @@
+package com.team1.monew.notification.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.team1.monew.config.QueryDslConfig;
+import com.team1.monew.notification.dto.ResourceType;
+import com.team1.monew.notification.entity.Notification;
+import com.team1.monew.user.entity.User;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.context.annotation.Import;
+
+@DataJpaTest
+@Import(QueryDslConfig.class) // 필요 시 QueryDSL 설정 포함
+class NotificationRepositoryTest {
+
+  @Autowired
+  private NotificationRepository notificationRepository;
+
+  @Autowired
+  private TestEntityManager entityManager;
+
+  @Test
+  @DisplayName("markAllAsConfirmedByUserId() 성공")
+  void markAllAsConfirmedByUserId_shouldUpdateConfirmedFieldToTrue() {
+    // given
+    User user = User.builder()
+        .email("test@example.com")
+        .nickname("testuser")
+        .password("testpassword")
+        .build();
+    entityManager.persist(user);
+
+
+    Notification notification1 = Notification.builder()
+        .user(user)
+        .content("알림1")
+        .resourceType(ResourceType.COMMENT.getName())
+        .resourceId(11L)
+        .build();
+    entityManager.persist(notification1);
+
+    Notification notification2 = Notification.builder()
+        .user(user)
+        .content("알림2")
+        .resourceType(ResourceType.COMMENT.getName())
+        .resourceId(12L)
+        .build();
+    entityManager.persist(notification2);
+
+    entityManager.flush();
+    entityManager.clear(); // 1차 캐시 제거
+
+    // when
+    notificationRepository.markAllAsConfirmedByUserId(user.getId());
+    entityManager.flush();
+    entityManager.clear(); // 다시 쿼리 실행하게끔 캐시 비움
+
+    // then
+    List<Notification> all = notificationRepository.findAll();
+    assertThat(all).hasSize(2);
+    assertThat(all).allMatch(Notification::isConfirmed);
+  }
+}
