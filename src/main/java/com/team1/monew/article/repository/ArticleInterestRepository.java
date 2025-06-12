0baf8e98cfbf8986c1f5eae1ee73169be1caecc1
nayu-yuna <dldldbsk@naver.com>
2025-06-12 17:06:07 +0900

    feat: KAN-333 관심사 변경 시 관련 기사 자동 연결 기능 구현 (#59)

--- src/main/java/com/team1/monew/article/repository/ArticleInterestRepository.java
+++ src/main/java/com/team1/monew/article/repository/ArticleInterestRepository.java
@@ -1,11 +1,25 @@
 package com.team1.monew.article.repository;
 
 import com.team1.monew.article.entity.ArticleInterest;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ArticleInterestRepository extends CrudRepository<ArticleInterest, Long> {
 
   void deleteByArticleId(Long articleId);
+
+    boolean existsByArticleIdAndInterestId(Long articleId, Long interestId);
+
+  @Modifying
+  @Query("DELETE FROM ArticleInterest ai WHERE ai.article.id = :articleId AND ai.interest.id = :interestId")
+  void deleteByArticleIdAndInterestId(@Param("articleId") Long articleId, @Param("interestId") Long interestId);
+
+  @Query("SELECT ai.article.id FROM ArticleInterest ai JOIN ai.article a " +
+          "WHERE ai.interest.id = :interestId AND " +
+          "(a.title LIKE %:keyword% OR a.summary LIKE %:keyword%)")
+  List<Long> findArticleIdsByInterestIdAndKeyword(@Param("interestId") Long id, @Param("keyword") String removedKeyword);
 }
