58f6a816909ec85d247f4ae3de1432b5c9ebf2a8
nayu-yuna <dldldbsk@naver.com>
2025-06-12 17:06:07 +0900

    feat: KAN-333 관심사 변경 시 관련 기사 자동 연결 기능 구현 (#59)

--- src/main/java/com/team1/monew/article/repository/ArticleRepository.java
+++ src/main/java/com/team1/monew/article/repository/ArticleRepository.java
@@ -1,11 +1,19 @@
 package com.team1.monew.article.repository;
 
+import com.team1.monew.article.dto.ArticleDto;
 import com.team1.monew.article.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
   boolean existsBySourceUrl(String link);
+
+  @Query("SELECT a FROM Article a WHERE a.title LIKE %:titleKeyword% OR a.summary LIKE %:summaryKeyword%")
+  List<Article> findByTitleContainingOrSummaryContaining(@Param("titleKeyword") String titleKeyword, @Param("summaryKeyword") String summaryKeyword);
 }
