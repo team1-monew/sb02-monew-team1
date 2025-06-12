d402d5ebe757e3e0ea80225c615e96efa62661a1
Leekb0804 <lkb8713@gmail.com>
2025-06-12 09:49:19 +0900

    feat: KAN-325 레포지토리 작성

--- src/main/java/com/team1/monew/notification/repository/NotificationRepository.java
+++ src/main/java/com/team1/monew/notification/repository/NotificationRepository.java
@@ -1,6 +1,7 @@
 package com.team1.monew.notification.repository;
 
 import com.team1.monew.notification.entity.Notification;
+import java.time.LocalDateTime;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -24,4 +25,10 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
       " SET n.confirmed = true " +
       " WHERE n.id = :notificationId")
   void markAsConfirmedByNotificationId(@Param("notificationId") Long notificationId);
+
+  @Modifying
+  @Transactional
+  @Query("DELETE FROM Notification n " +
+      " WHERE n.confirmed = true AND n.updatedAt < :cutoffDate")
+  int deleteConfirmedBefore(LocalDateTime cutoffDate);
 }
