afa4e7866259aa76c67d6780b00384536ab10c5c
manleKim <96470835+manleKim@users.noreply.github.com>
2025-06-14 16:26:25 +0900

    fix: KAN-370 테스트 코드 버그 수정 (#71)
    Co-authored-by: GogiDosirak <154963143+gogidosirak@users.noreply.github.com>

--- src/main/java/com/team1/monew/MonewApplication.java
+++ src/main/java/com/team1/monew/MonewApplication.java
@@ -9,9 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 public class MonewApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MonewApplication.class, args);
 	}
-
 }
