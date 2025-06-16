2f057ef49a64a64ba2acad33f018e9100ccd0641
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-06-16 09:19:27 +0900

    feat: KAN-373 활동 내역 스케쥴링 재시도 로직 추가 (#76)
    * feat: KAN-385 댓글 활동 내역 스케쥴링 재시도 로직 구현
    
    * feat: KAN-386 댓글 좋아요 활동 내역 스케쥴링 재시도 로직 구현
    
    * feat: KAN-374 구독 활동 내역 스케쥴링 재시도 로직 구현
    
    * feat: KAN-375 기사 조회 활동 내역 스케쥴링 재시도 로직 구현
    
    * test: KAN-388 테스트 코드 수정
    
    ---------
    
    Co-authored-by: manleKim <kk410910@naver.com>

--- src/main/java/com/team1/monew/useractivity/scheduler/service/ArticleViewActivityBatchService.java
+++ src/main/java/com/team1/monew/useractivity/scheduler/service/ArticleViewActivityBatchService.java
@@ -1,5 +1,7 @@
 package com.team1.monew.useractivity.scheduler.service;
 
+import static com.team1.monew.exception.ErrorCode.MAX_RETRY_EXCEEDED;
+
 import com.mongodb.MongoBulkWriteException;
 import com.mongodb.client.model.BulkWriteOptions;
 import com.mongodb.client.model.ReplaceOneModel;
@@ -9,20 +11,20 @@ import com.team1.monew.article.dto.ArticleViewDto;
 import com.team1.monew.article.entity.ArticleView;
 import com.team1.monew.article.mapper.ArticleViewMapper;
 import com.team1.monew.article.repository.ArticleViewRepository;
-import com.team1.monew.subscription.dto.SubscriptionDto;
-import com.team1.monew.subscription.entity.Subscription;
-import com.team1.monew.subscription.mapper.SubscriptionMapper;
+import com.team1.monew.exception.RestException;
 import com.team1.monew.user.entity.User;
 import com.team1.monew.user.repository.UserRepository;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.Document;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -34,14 +36,17 @@ public class ArticleViewActivityBatchService {
   private final ArticleViewRepository articleViewRepository;
   private final MongoTemplate mongoTemplate;
   private final UserRepository userRepository;
+  private final RetryTemplate retryTemplate;
 
   @Transactional
   public void syncAll() {
     List<User> users = userRepository.findAll();
     List<WriteModel<Document>> operations = new ArrayList<>();
+    List<Long> failedUserIds = new ArrayList<>();
 
     for (User user : users) {
-      List<ArticleView> articleViews = articleViewRepository.findValidArticleViewsByUserIdOrderByCreatedAt(user.getId());
+      List<ArticleView> articleViews = articleViewRepository.findValidArticleViewsByUserIdOrderByCreatedAt(
+          user.getId());
       List<ArticleViewDto> articleViewDtos = articleViews.stream()
           .map(articleView -> ArticleViewMapper.toDto(articleView, 0L))
           .toList();
@@ -63,17 +68,40 @@ public class ArticleViewActivityBatchService {
       operations.add(replaceModel);
     }
 
-    try {
-      if (!operations.isEmpty()) {
-        mongoTemplate.getCollection("article_view_activities")
-            .bulkWrite(operations, new BulkWriteOptions().ordered(false));
-        log.info("총 {}건의 ArticleViewActivity 문서가 bulkWrite 되었습니다.", operations.size());
+    if (!operations.isEmpty()) {
+      try {
+        retryTemplate.execute(context -> {
+          mongoTemplate.getCollection("article_view_activities")
+              .bulkWrite(operations, new BulkWriteOptions().ordered(false));
+          log.info("총 {}건의 ArticleViewActivity 문서가 bulkWrite 되었습니다.", operations.size());
+          return null;
+        });
+      } catch (Exception e) {
+        // 로그 분석을 위해 추적 가능한 실패한 유저 ID 추출
+        if (e instanceof MongoBulkWriteException mongoEx) {
+          List<Long> bulkFailedUserIds = mongoEx.getWriteErrors().stream()
+              .map(error -> {
+                int index = error.getIndex();
+                Document failedDoc = ((ReplaceOneModel<Document>) operations.get(
+                    index)).getReplacement();
+                return failedDoc.getLong("_id");
+              })
+              .toList();
+          failedUserIds.addAll(bulkFailedUserIds);
+        }
+        log.error("ArticleViewActivity bulkWrite 재시도 실패 - 최대 재시도 횟수 초과, userIds: {}",
+            failedUserIds, e);
+        throw new RestException(
+            MAX_RETRY_EXCEEDED,
+            Map.of(
+                "detail", "기사 조회 활동 내역 bulkWrite 최종 실패",
+                "skippedUserIds", failedUserIds.toString()
+            ));
       }
-    } catch(MongoBulkWriteException e) {
-      log.error("ArticleViewActivity bulkWrite 실패 - 일부 문서만 저장되었습니다.", e);
-      e.getWriteErrors().forEach(err ->
-          log.warn("실패 문서 index: {}, 코드: {}, 메시지: {}",
-              err.getIndex(), err.getCode(), err.getMessage()));
+      log.info("[배치 종료] 기사 조회 활동 내역 MongoDB 동기화 완료");
+      log.info("[요약] 전체 사용자 수: {}, 처리 성공 수: {}, 실패 사용자 수: {}",
+          users.size(), operations.size(), failedUserIds.size());
     }
   }
 }
+
